90991984851dbfb5136bcdfbe55ab2ef8f4f2ad6
gates1de <g031i043@gmail.com>
2021-01-31 17:15:56 +0900

    :bug: Fix bug that it does not set env value on client side

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 const path = require('path')
 
 module.exports = {
+  env: {
+    APP_ENV: process.env.APP_ENV
+  },
   webpack: config => {
     // path setting
     config.resolve.modules = [...config.resolve.modules, './src']
