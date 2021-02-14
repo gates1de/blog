372dd98b40c591ad6ae3253ca78a96a0997c8a5f
gates1de <g031i043@gmail.com>
2021-02-14 19:43:40 +0900

    :page_facing_up: Rename image

--- src/constants/image-path.ts
+++ src/constants/image-path.ts
@@ -1,3 +1,3 @@
 export const ogpImages: { [key: string]: string } = {
-  'd9c062b3-f4a2-47a6-8ae8-7b7616c6fc0b': `${process.env.SITE_URL}/static/images/ogp/d9c062b3-f4a2-47a6-8ae8-7b7616c6fc0b.jpg`,
+  '42b633a8-5bc3-46d2-8db7-e40ccbafd0f4': `${process.env.SITE_URL}/static/images/ogp/42b633a8-5bc3-46d2-8db7-e40ccbafd0f4.jpg`,
 }
