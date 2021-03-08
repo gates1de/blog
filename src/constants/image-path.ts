ab613266aebd6c8958055c274fdadf20b33b8197
gates1de <g031i043@gmail.com>
2021-03-08 19:23:57 +0900

    :page_facing_up: Add ogp image for 8845134d-4beb-4edf-bfab-699537abd7ce post

--- src/constants/image-path.ts
+++ src/constants/image-path.ts
@@ -1,3 +1,4 @@
 export const ogpImages: { [key: string]: string } = {
   '42b633a8-5bc3-46d2-8db7-e40ccbafd0f4': `${process.env.SITE_URL}/static/images/ogp/42b633a8-5bc3-46d2-8db7-e40ccbafd0f4.jpg`,
+  '8845134d-4beb-4edf-bfab-699537abd7ce': `${process.env.SITE_URL}/static/images/ogp/8845134d-4beb-4edf-bfab-699537abd7ce.jpg`,
 }
